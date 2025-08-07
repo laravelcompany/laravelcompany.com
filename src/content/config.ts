2cfe98e471ca12c6a962c1d8e70de44f4a1a0098
Bogdanel Stefan <bogdan.izdrail@gmail.com>
2025-08-07 12:57:25 +0100

    Chores added more content on page product

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 
 
-// Define collections
-const blog = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    author: z.string(),
-    authImage: z.string().url(),
-    image: z.string(),
-    summary: z.string(),
-    type: z.enum( ['Article', 'Tutorial', 'Magazine']),
-  }),
-});
 
 const software = defineCollection({
   type: 'content',
@@ -24,43 +11,8 @@ const software = defineCollection({
   }),
 });
 
-const docs = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-  }),
-});
-
-export const products = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    image: z.string().optional(),
-    brand: z.string().optional(),
-    price: z.number().optional(),
-    currency: z.string().optional(),
-    availability: z.string().optional(),
-    category: z.string().optional(),
-    sku: z.string().optional(),
-    mpn: z.string().optional(),
-    gtin: z.string().optional(),
-    manufacturer: z.string().optional(),
-    rating: z.object({
-      value: z.number(),
-      reviewCount: z.number(),
-      bestRating: z.number().optional(),
-      worstRating: z.number().optional()
-    }).optional(),
-    publishedDate: z.string().optional(),
-    modifiedDate: z.string().optional()
-  })
-});
-
 // Export all collections
 export const collections = {
-  blog: blog,
   software: software,
-  docs: docs,
-  products: products,
 };
 
