c51288d9bc57f817ea6bf90329ffb441190d1f83
Bogdanel Stefan <bogdan.izdrail@gmail.com>
2025-08-07 11:44:31 +0100

    Chores - updates and added new product

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwind from "@astrojs/tailwind";
 
 import cloudflare from '@astrojs/cloudflare';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   output: 'server',
   adapter: cloudflare(),
 });
